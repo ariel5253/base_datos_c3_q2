d97635f79006d6489aab83301294077c37cea661
Jesús Ariel González Bonilla <ariel5253@hotmail.com>
2024-10-30 07:10:45 -0500

    cambios

--- caso1.sql
+++ caso1.sql
@@ -1,8 +1,3 @@
-# base_datos_c3_q2
-Quiz corte 3 - Base de datos
-
-
-```sql
 -- Obtener de manera ordenada la lista de vistas y modulos a los que tiene acceso un rol en estado activo. 
 SELECT 
     r.name AS rol,
@@ -18,6 +13,3 @@ FROM
     INNER JOIN view v ON mv.view_id = v.id 
 WHERE r.state = true
 ORDER BY r.name
-```
-![Sin](img/sin.png)
-![Con](img/con-data.png)
